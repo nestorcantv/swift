cfef7b0dd4422e10c23a2681d2a1ba08dfb101bb
Néstor Moreno <ultranestor@hotmail.com>
2022-04-12 15:07:10 -0500

    Se termina de ver tuplas

--- 01_Tuplas.playground/Contents.swift
+++ 01_Tuplas.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let http404Error = (404, "Página no encontrada")
+let (statusCode,statusMessage) = http404Error
+print("\(statusCode)")
+let http200Status = (statusCode: 200,statusMessage: "Conectado")
+print(http200Status.statusMessage)
+let anotherTupla = ("Néstor","Moreno")
+print("\(anotherTupla.0)")
+
+let nestorInformation = (nombre: "Néstor", edad: 29)
+print("Hola soy \(nestorInformation.nombre) y tengo \(nestorInformation.edad)")

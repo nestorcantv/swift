7af37eaee344ab01577ab64d020e802906a4b9b5
Néstor Moreno <ultranestor@hotmail.com>
2022-04-27 20:36:31 -0500

    Se crea el primer proyecto

--- 3cursoSwift/Practice.playground/Contents.swift
+++ 3cursoSwift/Practice.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+class Product{
+    var name: String
+    var varCode: Int
+    var price: Double
+    var description: String?
+    init(name: String,
+         varCode: Int,
+         price: Double) {
+        self.name = name
+        self.varCode = varCode
+        self.price = price
+    }
+    
+    func ItemsPrinting(){
+        print(self.name)
+        print(self.varCode)
+        print(self.price)
+        if let itemDescription = self.description{
+            print(itemDescription)
+        }
+        
+    }
+    func specificItem(it: Any){
+        print(it)
+    }
+}
+var leche = Product(name: "Leche", varCode: 23424534, price: 20.50)
+var arroz = Product(name: "Arroz", varCode: 567252, price: 25.0)
+leche.description = "Leche entera"
+var products = [Product]()
+products.append(leche)
+products.append(arroz)
+
+
+
+for raw in products{
+    raw.ItemsPrinting()
+    raw.specificItem(it: raw.price)
+}
---
+++ Practica1/Practica1/Practica1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practica1App.swift
+//  Practica1
+//
+//  Created by mac on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Practica1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

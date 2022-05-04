ef55ca77a3f59f1ed027cdc1d84356e5b5654271
Néstor Moreno <ultranestor@hotmail.com>
2022-05-03 22:25:45 -0500

    Se comienza con el proyecto del curso de storyboards

---
+++ PlatziFinanzas/PlatziFinanzas/Application/Main/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by mac on 03/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
